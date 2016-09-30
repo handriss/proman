b923a1ad1db0442fe07e830fbe37623d46e07c7a
handriss <andrashinkel@gmail.com>
2016-09-30 23:27:51 +0200

    sidebar colored

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,15 @@ $("#menu-toggle").click(function(event) {
         event.preventDefault();
         $("#wrapper").toggleClass("active");
 });
+
+$("#first-link").click(function() {
+    console.log("first-link clicked");
+});
+
+$("#second-link").click(function() {
+    console.log("second-link clicked");
+});
+
+$("#third-link").click(function() {
+    console.log("third-link clicked");
+});
